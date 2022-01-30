43a7a7fe7fe699d61852436a9a02ed042e1ce224
Chitra <chitradevi.rajendran@gmai.com>
2022-01-30 18:55:55 +0100

    ddfsdf

--- src/services/TimesheetService.js
+++ src/services/TimesheetService.js
@@ -4,23 +4,53 @@ const EMPLOYEE_API_BASE_URL = "https://hello-cloud-run-2ia6hqkrja-uc.a.run.app";
 class TimesheetService {
 
     getTimesheets(){
-        return axios.get(EMPLOYEE_API_BASE_URL+ '/getTimes' );
+        return axios.get(EMPLOYEE_API_BASE_URL+ '/getTimes' ,{
+            headers: {
+                'Access-Control-Allow-Origin' : '*',
+                'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+              },
+      
+        });
     }
 
     createTimesheet(timesheet){
-        return axios.post(EMPLOYEE_API_BASE_URL, timesheet);
+        return axios.post(EMPLOYEE_API_BASE_URL, timesheet,{
+            headers: {
+                'Access-Control-Allow-Origin' : '*',
+                'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+              },
+      
+        }) ;
     }
 
     getTimesheetById(timesheetId){
-        return axios.get(EMPLOYEE_API_BASE_URL + '/' + timesheetId);
+        return axios.get(EMPLOYEE_API_BASE_URL + '/' + timesheetId,{
+            headers: {
+                'Access-Control-Allow-Origin' : '*',
+                'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+              },
+      
+        });
     }
 
     updateTimesheet(timesheet, timesheetId){
-        return axios.put(EMPLOYEE_API_BASE_URL + '/' + timesheetId, timesheet);
+        return axios.put(EMPLOYEE_API_BASE_URL + '/' + timesheetId, timesheet,{
+            headers: {
+                'Access-Control-Allow-Origin' : '*',
+                'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+              },
+      
+        });
     }
 
     deleteTimesheet(timesheetId){
-        return axios.delete(EMPLOYEE_API_BASE_URL + '/' + timesheetId);
+        return axios.delete(EMPLOYEE_API_BASE_URL + '/' + timesheetId,{
+            headers: {
+                'Access-Control-Allow-Origin' : '*',
+                'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+              },
+      
+        });
     }
 }
 
